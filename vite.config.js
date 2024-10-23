2b72136315e32545bd9350a80861bf4fe77dbab2
AlexisRSM <alexismendes98@hotmail.com>
2024-10-23 22:54:31 +0100

    On Render port bind

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Bind to 0.0.0.0 to make it accessible externally
+    port: process.env.PORT || 3000  // Use Render's dynamically assigned port
+  }
 })
